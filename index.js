7e3742b2a9f87b77470a3946c51e108c46ba5311
ensignjava <109529105+maijako@users.noreply.github.com>
2022-12-18 10:55:12 +0200

    subtracted consecutive months from previous and stored in a new variable

--- index.js
+++ index.js
@@ -88,17 +88,17 @@ var finances = [
 ];
 
 //THE TASK INSTRUCTIONS
-// You have been given a dataset composed of arrays with two fields, Date and Profit/Losses.
-// Your task is to write JavaScript code that analyzes the records to calculate each of the following:
-    // * The total number of months included in the dataset.
-    // * The net total amount of Profit/Losses over the entire period.
+// // You have been given a dataset composed of arrays with two fields, Date and Profit/Losses.
+// // Your task is to write JavaScript code that analyzes the records to calculate each of the following:
+//     // * The total number of months included in the dataset.
+//     // * The net total amount of Profit/Losses over the entire period.
     // * The average of the **changes** in Profit/Losses over the entire period.
     //   * You will need to track what the total change in profits are from month to month and then find the average.
     //   * (`Total/Number of months`)
     // * The greatest increase in profits (date and amount) over the entire period.
     // * The greatest decrease in losses (date and amount) over the entire period.
 
-// When you open your code in the browser your resulting analysis should look similar to the following:
+// Example:
     //   ```text
     //   Financial Analysis
     //   ----------------------------
@@ -109,10 +109,7 @@ var finances = [
     //   Greatest Decrease in Profits: Sep-2013 ($-2196167)
     //   ```
 
-// Your final code should print the analysis to the console.
-
 // **Hints:**
-    // * You will need to do some research on your own for this project!
     // * Remember, in order to combine strings and variables in the console you will need to use **concatenation**.
     // * How do you only print to the nearest 100th in JavaScript?
 
@@ -170,10 +167,17 @@ for(var i=0; i<lossesList.length; i++){
 }
 console.log("Total losses: $"+totalLosses);
 
+
+// count differences in profits each month, store in a differences variable
+var differences = financeNum.slice(1).map((x,i)=> x-financeNum[i]);
+
+// differences.forEach((x,i) => console.log(
+//    `Profits from month ${i+1} to month ${i+2} increased by ${x}`
+// ));
+console.log(differences);
+
 //  count average change, store in a ChangeVariable
 //  console.log "Average Change: "+ChangeVariable
-
-
 
 //  count greatest increase in profits, store in IncreaseVariable
 //  console.log "Greatest Increase in Profits: "+IncreaseVariable
